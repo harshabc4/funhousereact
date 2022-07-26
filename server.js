93062a54154d6846b62c92e1b0383797c4cb20b6
Brian Harsha <harshabc4@gmail.com>
2022-07-26 16:03:38 -0400

    build path

--- server.js
+++ server.js
@@ -76,6 +76,19 @@ app.put("/put/:id", (req, res) => {
   );
 });
 
+// // TEST START
+// // Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+// // TEST END
+
 app.listen(PORT, function () {
   console.log("Express is running");
 });
