5acdc60609021c1225df1cef0ccf009649bdd997
B <harshabc4@gmail.com>
2023-04-02 14:16:29 -0400

    remove var

--- client/src/components/SocialMedia/SocialMedia.js
+++ client/src/components/SocialMedia/SocialMedia.js
@@ -2,41 +2,6 @@ import React, { useState, useEffect, useRef } from 'react'
 import axios from 'axios';
 import Feed from '../Feed/Feed';
 
-// export default function SocialMedia() {
-
-// const [gameBoard] = React.useState(
-//    Array.from({ length: apiResponse.length })
-// );
-
-// const requestPostButton = document.getElementById("requests-posts");
-// requestPostButton.addEventListener("click", requestSocialMediaPosts);
-
-// requestSocialMediaPosts = async () => {
-//    fetch('https://graph.instagram.com/v16.0/17841400385468108?fields=id,username,media&access_token=IGQVJXVVBuZAWhrQzJzZAlFJZA1FRWERoRjZAXdHdNTFlEUkh4NExSTTRjeHFZAN1kwbmdJRmlxbVVHdzBxRTVBQklqdGk5cUdmcmtzLUNIMFdHa1F5R0tCOTFkTUQ0bXBpNFlxaHYyVERn',
-//       {
-//          method: 'POST',
-//          body: form,
-//       }).then(response => {
-//          console.log(response)
-//       }).catch(error => {
-//          console.error(error);
-//       })
-// }
-
-// foreach(item in apiResponse)
-// {
-//    gameBoard.push(item);
-// }
-
-
-// return (
-//    <div>
-//       <button>Refresh token</button>
-//       <button id="request-posts">Load social posts</button>
-//    </div>
-// )
-// };
-
 const SocialMedia = ({ token, ...props }) => {
    const [feeds, setFeedsData] = useState([])
    //use useRef to store the latest value of the prop without firing the effect
