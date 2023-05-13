bfced8ac916caa0a5d9f0966cb38cbcf55bed0f9
B <harshabc4@gmail.com>
2023-05-13 11:21:14 -0400

    delete old key; update schema to have expiration and creation date;

--- server.js
+++ server.js
@@ -7,40 +7,67 @@ const PORT = process.env.PORT || 2121;
 const cron = require('node-cron');
 require("dotenv").config();
 
-
-
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // connect to mongobd
 let db,
-  dbConnectionStr = process.env.DB_STRING,
-  dbName = "logitrestructure";
+  dbConnectionStr = process.env.DB_STRING
 
 mongoose.connect(dbConnectionStr);
 
 // data schema
-const itemSchema = {
-  title: String,
-  description: String,
-};
-
 const keySchema = {
   title: String,
   value: String,
   expiresIn: Number,
+  createdOn: Date,
+  expiresOn: Date
 }
 
 // data model
-const Item = mongoose.model("Item", itemSchema, "materials");
-const Keytest = mongoose.model("Keytest", keySchema, "key");
+const Key = mongoose.model("Key", keySchema, "Keys");
+
+// addStarterKey();
+
+// async function addStarterKey() {
+//   const lastKey = await getLastKey();
+//   await addKeyToDbWithoutRefresh(lastKey);
+// }
+
+// async function addKeyToDbWithoutRefresh(lastKey) {
+//   console.log("addKeyToDb");
+//   let oldTitle = lastKey.title;
+//   let titleAsArray = oldTitle.split('');
+//   let newTitle = "Key" + (Number(titleAsArray[titleAsArray.length - 1]) + 1);
+//   let todaysDate = Date.now();
+//   let expirationDate = new Date(todaysDate + lastKey.expiresIn * 1000);
+//   Key.create({ title: newTitle, value: lastKey.value, expiresIn: lastKey.expiresIn, createdOn: todaysDate, expiresOn: expirationDate }, function (err, doc) {
+//     if (err) return console.error(err);
+//     console.log(doc);
+//   })
+//   Key.countDocuments({}, function (err, count) {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       if (count > 5) {
+//         Key.findOneAndDelete({}, { sort: { created_at: 1 } }, function (err, deletedDoc) {
+//           if (err) {
+//             console.error(err);
+//           } else {
+//             console.log('Successfully deleted the oldest document:', deletedDoc);
+//           }
+//         });
+//       }
+//     }
+//   })
+// }
 
 function getLastKey() {
   console.log("getLastKey");
   return new Promise((resolve, reject) => {
-    Keytest.findOne().sort({ _id: -1 }).exec((err, lastKey) => {
+    Key.findOne().sort({ _id: -1 }).exec((err, lastKey) => {
       if (err) {
         reject(err);
       }
@@ -65,9 +92,26 @@ async function addKeyToDb(result, oldTitle) {
   console.log("addKeyToDb");
   let titleAsArray = oldTitle.split('');
   let newTitle = "Key" + (Number(titleAsArray[titleAsArray.length - 1]) + 1)
-  Keytest.create({ title: newTitle, value: result.access_token, expiresIn: result.expires_in }, function (err, doc) {
+  let todaysDate = Date.now();
+  let expirationDate = new Date(todaysDate + result.expires_in * 1000);
+  Key.create({ title: newTitle, value: result.access_token, expiresIn: result.expires_in, createdOn: todaysDate, expiresOn: expirationDate }, function (err, doc) {
     if (err) return console.error(err);
     console.log(doc);
+  })
+  Key.countDocuments({}, function (err, count) {
+    if (err) {
+      console.error(err);
+    } else {
+      if (count > 5) {
+        Key.findOneAndDelete({}, { sort: { created_at: 1 } }, function (err, deletedDoc) {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log('Successfully deleted the oldest document:', deletedDoc);
+          }
+        });
+      }
+    }
   })
 }
 
@@ -85,7 +129,7 @@ async function refreshKeyAndAddToDb(oldToken) {
     });
 }
 
-console.log(process.env.RAILWAY_ENVIRONMENT);
+console.log("environment " + process.env.RAILWAY_ENVIRONMENT);
 if (process.env.RAILWAY_ENVIRONMENT == 'production') {
   cron.schedule('0 6 * * 6', () => {
 
@@ -99,12 +143,6 @@ if (process.env.RAILWAY_ENVIRONMENT == 'production') {
 
 
 // read route
-app.get("/showMaterials", (req, res) => {
-  Item.find()
-    .then((items) => res.json(items))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
-
 app.get("/getPosts", (req, res) => {
   fetchInstagramPost()
     .then(data => {
@@ -130,50 +168,6 @@ async function fetchInstagramPost() {
   }
 }
 
-// create route
-app.post("/addMaterial", (req, res) => {
-  const newItem = new Item({
-    title: req.body.title,
-    description: req.body.description,
-  });
-  newItem
-    .save()
-    .then((item) => console.log(item))
-    .catch((err) => res.status(400).json("Error " + err));
-});
-
-// delete route
-app.delete("/delete/:id", (req, res) => {
-  const id = req.params.id;
-
-  Item.findByIdAndDelete({ _id: id }, (req, res, err) => {
-    if (!err) {
-      console.log("Item deleted");
-    } else {
-      console.log(err);
-    }
-  });
-});
-
-// update route
-app.put("/put/:id", (req, res) => {
-  const updatedItem = {
-    title: req.body.title,
-    description: req.body.description,
-  };
-  Item.findByIdAndUpdate(
-    { _id: req.params.id },
-    { $set: updatedItem },
-    (req, res, err) => {
-      if (!err) {
-        console.log("Item updated");
-      } else {
-        console.log(err);
-      }
-    }
-  );
-});
-
 // // TEST START
 // // Accessing the path module
 const path = require("path");
@@ -190,53 +184,6 @@ app.get("*", function (request, response) {
 app.listen(PORT, function () {
   console.log("Express is running");
   console.log(PORT);
-});
-
-const key = '';
-
-console.log("adsf");
-
-app.post('/', (request, response) => {
-  db.collection('key').insertOne({
-    brand: "zzz",
-    product: "aaa"
-  })
-    .then(result => {
-      console.log('Material Added')
-      // response.redirect('/materials')
-    })
-    .catch(error => console.error(error))
-})
-
-app.get('/', (request, response) => {
-  // Sort array by amount
-  db.collection('materials').find().toArray()
-    .then(data => {
-      key = data
-      console.log(data)
-      console.log(key)
-      console.log(response)
-      console.log("adsf")
-      // response.render('index.ejs', { info: data })
-    })
-    .catch(error => console.error(error))
-})
-
-app.get('/', (request, response) => {
-  // Sort array by amount
-  db.collection('materials').find().sort({ amount: -1 }).toArray()
-    .then(data => {
-      console.log(response)
-      console.log(data)
-    })
-    .catch(error => console.error(error))
-})
-
-app.get("/", (req, res) => {
-  Item.find()
-    .then((items) => res.json(items))
-    .then(console.log(res))
-    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 let limit = 8;
